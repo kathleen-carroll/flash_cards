da8dc9aca1e13a4c61f4d7196e1583b1bb5605a7
Kathleen Carroll <khakicarroll@comcast.net>
2019-11-20 17:38:05 -0700

    Update deck and test file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,27 @@
 class Deck
-  attr_reader :card, :cards
-  
-  def initialize(card)
-    @card = card
-    @cards = []
+  attr_reader :deck, :cards
+
+  def initialize(cards)
+    @cards = cards
+    @deck = []
+  end
+
+  def add_card(cards)
+    @deck << cards
+  end
+
+  def count
+    @deck.count
+  end
+
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      #require "pry"; binding.pry
+       category == card.category
+    end
   end
 end
+
+# cards = [@card1, @card2, @card3]
+# deck = Deck.new(@cards)
+# p deck.cards_in_category(:STEM)
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,8 +9,24 @@ class DeckTest < Minitest::Test
     @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
   end
+
   def test_it_exists
-    
+    assert_instance_of Deck, @deck
+  end
+
+  def test_if_cards_works
+    #@card1.add_card
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_the_card_count
+    assert_equal @cards.count, @deck.cards.count
+  end
+
+  def test_cards_in_category_stem
+    assert_equal [@card2, @card3], @deck.cards_in_category(:STEM)
   end
 end
