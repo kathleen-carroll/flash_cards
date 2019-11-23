a8650ae9c19c734cd3a9fee9fb8f7cd25952406b
Kathleen Carroll <khakicarroll@comcast.net>
2019-11-23 14:10:38 -0700

    cleaned runner file of excess comments

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -14,13 +14,6 @@ require './lib/round'
 
 puts "Welcome! You are playing with #{@cards.count} cards."
 puts "----------------------------------------------------"
-#puts "You are on card #{@round.num + 1} out of #{@cards.count}"
-
-#p @round.deck.cards[0]
-#require "pry"; binding.pry
-#p @round.deck.cards #returns whole card
-
-#p @round.turns returns blank array
 
 def start #use until loop until deck is empty
   #use @round.deck.cards to loop through
@@ -36,31 +29,7 @@ def start #use until loop until deck is empty
   @percent_category << @round.percent_correct_by_category(@category[@round.num - 1])
 
   puts @turn.feedback
-
   end
-
-  #p @round.take_turn(guess).turn
-  #p  @round.num
-  puts "******** Game over! ********"
-  puts "You had #{@round.number_correct} correct guesses out of #{@cards.count} for a total score of #{(@round.percent_correct).to_i}%."
-#require "pry"; binding.pry
-  @number = (1..@cards.count).to_a
-  #@number.each do |number|
-    #@number << (1..@cards.count).to_a
-  # @cards.each do
-  #p @cards.count
-  puts @category[0]
-  #puts @round.count
-  # @round.turns.find_all do |turn|
-  #   require "pry"; binding.pry
-  #   puts @round.turns.category
-  #   puts @round.percent_correct_by_category(@category[@round.num])
-  #   puts @turn.card.category
-  # end
-  #puts @round.percent_correct_by_category(@turn.card.category)
-  #puts (@turn.card.category)
-  #puts @category
-
 end
 
 start
@@ -72,11 +41,3 @@ puts "You had #{@round.number_correct} correct guesses out of #{@cards.count} fo
 @category.each do |category|
 puts "#{category} - #{(@round.percent_correct_by_category(category)).to_i}% correct"
 end
-
-#require "pry"; binding.pry
-#puts @round.percent_correct_by_category(@turn.card.category)
-#puts (@turn.card.category)
-
-#require "pry"; binding.pry
-
-#p @round.current_card
